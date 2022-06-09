2e63053b2d40231731a7205897c6732d8636359d
Alex Gervacio <alexgrvc@mesoamericana.edu.gt>
2022-06-09 16:31:50 -0600

    Actualizado

--- Pacientes.aspx.cs
+++ Pacientes.aspx.cs
@@ -20,21 +20,26 @@ namespace Consultorio_Medico
         protected void Button1_Click(object sender, EventArgs e)
         {
             Pacientes paciente = new Pacientes();
-            paciente.DPI = TextBoxDPIPaciente.Text;
-            paciente.Nombre = TextBoxNombre.Text;
-            paciente.Apellido = TextBoxApellido.Text;
-            paciente.Direccion = TextBoxDireccion.Text;
+            paciente.DPI = TextBoxDPIPaciente.Text = "";
+            paciente.Nombre = TextBoxNombre.Text = "";
+            paciente.Apellido = TextBoxApellido.Text = "";
+            paciente.Direccion = TextBoxDireccion.Text = "";
             paciente.Fecha_de_Nacimiento = CalendarNacimiento.SelectedDate;
-            paciente.Telefono = TextBoxTelefono.Text;
+            paciente.Telefono = TextBoxTelefono.Text = "";
             pacientes.Add(paciente);
             Guardar_Json();
-            Response.Write("<script>alert('Paciente correctamente registrado')</script>"); 
+            Response.Write("<script>alert('¡Paciente registrado con éxito!')</script>"); 
         }
         void Guardar_Json()
         {
             string json = JsonConvert.SerializeObject(pacientes);
             string archivo = Server.MapPath("Pacientes.json");
             File.WriteAllText(archivo, json);
+
+        }
+
+        protected System.Void Button1_Click(System.Object sender, System.EventArgs e)
+        {
 
         }
     }
